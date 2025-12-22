6fd5840da7fc80bbd8f49fbb26fe143b47f93aaa
Rummaninfo <birsabbir38@gmail.com>
2025-12-22 07:06:02 +0600

    add axios interceptor with auth token and auto logout on 401/403

--- src/Hook/UseAxios.jsx
+++ src/Hook/UseAxios.jsx
@@ -1,11 +1,47 @@
-import axios from 'axios';
-import React from 'react';
-  let axiosSecure = axios.create({
-    baseURL:"http://localhost:3000"
-  })
+import axios from "axios";
+import React, { useEffect } from "react";
+import UseAuth from "./UseAuth";
+import { useNavigate } from "react-router";
+let axiosSecure = axios.create({
+  baseURL: "http://localhost:3000",
+});
 const UseAxios = () => {
+  let { user, logOutUser } = UseAuth();
+  let navigate = useNavigate()
 
-    return axiosSecure
+  useEffect(() => {
+    // intercep
+   let reqInterceptor =  axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user.accessToken} `;
+      return config;
+    });React
+        let resInterceptor = axiosSecure.interceptors.response.use((response)=>{
+          return response
+        }, (error)=>{
+          console.log(error)
+
+          let statusCode = error.status 
+          if(statusCode === 401 || statusCode === 403 ){
+              logOutUser()
+              .then(res=>{
+                   console.log(res)
+                   navigate("register/login")
+              })
+              .catch(er=>{
+                console.log(er)
+              })
+          }
+          return Promise.reject(error)
+        })
+
+    return ()=>{
+      axiosSecure.interceptors.request.eject(reqInterceptor)
+      axiosSecure.interceptors.response.eject(resInterceptor)
+
+    }
+  }, [user, ]);
+
+  return axiosSecure;
 };
 
 export default UseAxios;
